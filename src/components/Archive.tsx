7a9425016bc13b397ee5867e2aaf734c51cb04ae
yousunzoo <dpfvmffjqm@gmail.com>
2023-07-26 00:10:06 +0900

    Design: archive link에 hover시 transition 처리

--- src/components/Archive.tsx
+++ src/components/Archive.tsx
@@ -10,19 +10,13 @@ function Archive() {
 			<h3 className='text-[#bfb746] font-bold text-3xl mb-6'>ARCHIVE</h3>
 			<div className='flex justify-between gap-10'>
 				<div className='w-1/2'>
-					<Link
-						href={GITHUB_LINK}
-						target='_blank'
-						className='flex italic w-fit gap-2 items-center text-white font-bold text-xl mb-4'>
+					<Link href={GITHUB_LINK} target='_blank' className='link active after:bg-[#FE27AB]'>
 						<span>Github</span>
 						<BiLinkExternal className='text-2xl mb-1' />
 					</Link>
 				</div>
 				<div className='w-1/2'>
-					<Link
-						href={BLOG_LINK}
-						target='_blank'
-						className='flex italic w-fit gap-2 items-center text-white font-bold text-xl mb-4'>
+					<Link href={BLOG_LINK} target='_blank' className='link active after:bg-[#49ce44]'>
 						<span>Blog</span>
 						<BiLinkExternal className='text-2xl mb-1' />
 					</Link>
